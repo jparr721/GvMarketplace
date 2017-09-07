f2fd4fb8bef7f44fbb6021654d3f52d6f81fb69b
Ronald Rounsifer <ronrounsifer@me.com>
2017-09-06 21:59:05 -0400

    Added buttons to browse page and changed up style a bit.

--- gvmarket/src/components/browse.js
+++ gvmarket/src/components/browse.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { List, ListItem, Button } from 'native-base';
 
-import { Header, PageView } from './common/index';
+import { Header, PageView, Card } from './common/index';
+
+const styles = {
+  buttonText: {
+    fontSize: 26,
+    color: '#34495e',
+  },
+  button: {
+    borderBottomWidth: 0.5,
+    borderColor: '#34495e',
+  },
+};
 
 class Browse extends Component {
   render() {
@@ -9,7 +21,40 @@ class Browse extends Component {
       <PageView>
         <Header headerText="Browse" />
         <View style={{ flex: 1 }}>
-          <Text>Browse Content</Text>
+        <ScrollView>
+          <View>
+            <Button large full light style={styles.button}><Text style={styles.buttonText}>For Sale</Text></Button>
+            <List>
+              <ListItem><Button full transparent><Text>Textbooks</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Computers</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Electronics</Text></Button></ListItem>
+            </List>
+          </View>
+          <View>
+            <Button large full light style={styles.button}><Text style={styles.buttonText}>Tutoring</Text></Button>
+            <List>
+              <ListItem><Button full transparent><Text>Biology</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Computer Science</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Physics</Text></Button></ListItem>
+            </List>
+          </View>
+          <View>
+            <Button large full light style={styles.button}><Text style={styles.buttonText}>Housing</Text></Button>
+            <List>
+              <ListItem><Button full transparent><Text>Apartments / Housing</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Roomates</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Subletting</Text></Button></ListItem>
+            </List>
+          </View>
+          <View>
+            <Button large full light style={styles.button}><Text style={styles.buttonText}>Jobs</Text></Button>
+            <List>
+              <ListItem><Button full transparent><Text>Part-time</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Full-time</Text></Button></ListItem>
+              <ListItem><Button full transparent><Text>Internships</Text></Button></ListItem>
+            </List>
+          </View>
+        </ScrollView>
         </View>
       </PageView>
     );
