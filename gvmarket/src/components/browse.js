9ba422eec71fd5b64e9cad4b8b2c111d89e21509
Ronald Rounsifer <ronrounsifer@me.com>
2017-09-10 19:57:11 -0400

    Added sample images to the layout. Must change before release.

--- gvmarket/src/components/browse.js
+++ gvmarket/src/components/browse.js
@@ -28,10 +28,6 @@ const styles = {
     height: 150,
     width: null,
   },
-  smCard: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
 };
 
 class Browse extends Component {
@@ -39,43 +35,41 @@ class Browse extends Component {
     return (
       <PageView>
         <View style={{ paddingTop: 20 }}>
-          <SearchBar
-            lightTheme
-            placeholder="Type Here..." />
-          </View>
+          <SearchBar lightTheme placeholder="Type Here..." />
+        </View>
         <View style={{ flex: 1 }}>
           <ScrollView>
             <View style={styles.categories}>
-              <Button large full light><Text style={styles.buttonText}>For Sale</Text></Button>
-              <ScrollView horizontal={true}>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Textbooks</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Computers</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Electronics</Text></Button></SmallCard>
-              </ScrollView>
-            </View>
-            <View style={styles.categories}>
               <Button large full light><Text style={styles.buttonText}>Tutoring</Text></Button>
               <ScrollView horizontal={true}>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Biology</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Computer Science</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Data Science</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Physics</Text></Button></SmallCard>
+                <SmallCard style={styles.smallerCard}><Button full transparent><Text style={styles.listText}>Biology</Text></Button><Image style={styles.images} source={require('./imgs/biology1.png')} /></SmallCard>
+                <SmallCard style={styles.smallerCard}><Button full transparent><Text style={styles.listText}>Computer Science</Text></Button><Image style={styles.images} source={require('./imgs/comp-sci2.png')} /></SmallCard>
+                <SmallCard style={styles.smallerCard}><Button full transparent><Text style={styles.listText}>Data Science</Text></Button><Image style={styles.images} source={require('./imgs/data-sci1.png')} /></SmallCard>
+                <SmallCard style={styles.smallerCard}><Button full transparent><Text style={styles.listText}>Physics</Text></Button><Image style={styles.images} source={require('./imgs/physics1.png')} /></SmallCard>
+                </ScrollView>
+            </View>
+            <View style={styles.categories}>
+              <Button large full light><Text style={styles.buttonText}>For Sale</Text></Button>
+              <ScrollView horizontal={true}>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Textbooks</Text></Button><Image style={styles.images} source={require('./imgs/tb1.jpg')} /></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Computers</Text></Button><Image style={styles.images} source={require('./imgs/comp1.jpg')} /></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Electronics</Text></Button><Image style={styles.images} source={require('./imgs/tv1.jpg')} /></SmallCard>
               </ScrollView>
             </View>
             <View style={styles.categories}>
               <Button large full light><Text style={styles.buttonText}>Housing</Text></Button>
               <ScrollView horizontal={true}>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Apartments / Housing</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Roomates</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Subletting</Text></Button></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Apartments / Housing</Text></Button><Image style={styles.images} source={require('./imgs/apt1.jpg')} /></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Roommates</Text></Button><Image style={styles.images} source={require('./imgs/roommates1.jpg')} /></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Subletting</Text></Button><Image style={styles.images} source={require('./imgs/sublease1.jpg')} /></SmallCard>
               </ScrollView>
             </View>
             <View>
               <Button large full light><Text style={styles.buttonText}>Jobs</Text></Button>
               <ScrollView horizontal={true}>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Part-time</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Full-time</Text></Button></SmallCard>
-                <SmallCard><Button full transparent><Text style={styles.listText}>Internships</Text></Button></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Part-time</Text></Button><Image style={styles.images} source={require('./imgs/part-time1.jpg')} /></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Full-time</Text></Button><Image style={styles.images} source={require('./imgs/full-time1.jpg')} /></SmallCard>
+                <SmallCard><Button full transparent><Text style={styles.listText}>Internships</Text></Button><Image style={styles.images} source={require('./imgs/internship1.jpg')} /></SmallCard>
               </ScrollView>
             </View>
           </ScrollView>
