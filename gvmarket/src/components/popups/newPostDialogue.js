bc1986568e705df20a3121d46a7feef86910fa9f
Jarred Parr <jparr721@gmail.com>
2017-10-09 14:24:21 -0400

    Almost done

--- gvmarket/src/components/popups/newPostDialogue.js
+++ gvmarket/src/components/popups/newPostDialogue.js
@@ -33,6 +33,34 @@ class NewPostDialogueContainer extends Component {
             </View>
             <View style={{padding: 10}}>
               <Text>Enter item details or use the camera!</Text>
+              <Card>
+                <CardSection>
+                  <Input
+                    placeholder="Title"
+                    label="Post title"
+                    onChangeText={(text) => this.props.onTitleChange(text)}
+                    value={this.props.postTitle}
+                  />
+                </CardSection>
+                <CardSection>
+                  <Input
+                    placeholder="Price"
+                    label="Price ($)"
+                    onChangeText={(text) => this.props.onPriceChange(text)}
+                    value={this.props.price}
+                  />
+                </CardSection>
+              </Card>
+              <Card>
+                <CardSection>
+                  <Input
+                    placeholder="Description (optional)"
+                    label="Description"
+                    onChangetext={(text) => this.props.onDescriptionChange(text)}
+                    value={this.props.description}
+                   />
+                </CardSection>
+              </Card>
             </View>
           </View>
         </Modal>
@@ -45,7 +73,6 @@ class NewPostDialogueContainer extends Component {
             color='#0d67a2'
           />
         </TouchableOpacity>
-        <
       </PageView>
     );
   }
