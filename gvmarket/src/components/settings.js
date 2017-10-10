7aef5313fc02baf8e39a84efcc4fd0270d1a7767
Ronald Rounsifer <ronrounsifer@me.com>
2017-10-10 12:09:46 -0400

    Added buttons to settings / Changed layout for settings

--- gvmarket/src/components/settings.js
+++ gvmarket/src/components/settings.js
@@ -2,14 +2,47 @@ import React, { Component } from 'react';
 import { View, Text, Image, ScrollView, StyleSheet } from 'react-native';
 import { PageView, Card, CardSection, Button } from './common';
 
+const styles = {
+  btns: {
+    marginTop: 50,
+  }
+
+}
+
 
 class Settings extends Component {
   render() {
     return (
       <PageView>
-        <CardSection>
-          <Button>Logout</Button>
-        </CardSection>
+          <View>
+          <ScrollView>
+            <View style={styles.btns}>
+                <Button>My posts</Button>
+            </View>
+
+            <View style={styles.btns}>
+                <Button>My watchlist</Button>
+            </View>
+
+            <View style={styles.btns}>
+                <Button>Refresh my location</Button>
+            </View>
+
+            <View style={styles.btns}>
+                <Button>Feedback</Button>
+            </View>
+
+            <View style={styles.btns}>
+                <Button>Logout</Button>
+            </View>
+
+            <View style={styles.btns}>
+              <Button>Delete account</Button>
+            </View>
+
+            </ScrollView>
+          </View>
+
       </PageView>
     );
   }
