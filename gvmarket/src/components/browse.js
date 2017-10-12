286d6e88c287f1f8553d1633b2269fa34ef91f69
Ronald Rounsifer <ronrounsifer@me.com>
2017-10-12 19:51:46 -0400

    Starting creating layout for browse page

--- gvmarket/src/components/browse.js
+++ gvmarket/src/components/browse.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, Dimensions } from 'react-native';
 
 import { PageView, SearchBar, Card, CardSection, Header } from './common/index';
 import { Button } from 'native-base';
@@ -11,15 +11,92 @@ const styles = {
     paddingTop: 20,
     flex: 1,
   },
+  oneThirdScreen: {
+    width: Dimensions.get('window').width / 3,
+    backgroundColor: '#FFF',
+  },
+  rightThird: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('window').width / 3,
+    backgroundColor: 'skyblue',
+
+
+  },
+  textStyle: {
+    paddingLeft: 10,
+    paddingTop: 10,
+    fontSize: 24,
+    color: '#007aff',
+  },
+  rightButtonText: {
+    color: '#007aff',
+  },
 };
 
 class Browse extends Component {
   render() {
     return (
-      <View syle={styles.container}>
+      <View style={styles.container}>
         <Header headerText="Browse">
           <NewPostDialogue />
         </Header>
+
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+          <View>
+            <Card>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <View style={styles.oneThirdScreen}>
+                  <Text style={styles.textStyle}>Tutoring</Text>
+                </View>
+                <View style={styles.oneThirdScreen} />
+                <View style={styles.oneThirdScreen}>
+                  <Button transparent full>
+                    <Text style={styles.rightButtonText}>Tutoring ></Text>
+                  </Button>
+                </View>
+              </View>
+            </Card>
+          </View>
+        </View>
+
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+          <View>
+            <Card>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <View style={styles.oneThirdScreen}>
+                  <Text style={styles.textStyle}>Housing</Text>
+                </View>
+                <View style={styles.oneThirdScreen} />
+                <View style={styles.oneThirdScreen}>
+                  <Button transparent full>
+                    <Text style={styles.rightButtonText}>Housing ></Text>
+                  </Button>
+                </View>
+              </View>
+            </Card>
+          </View>
+        </View>
+
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+          <View>
+            <Card>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <View style={styles.oneThirdScreen}>
+                  <Text style={styles.textStyle}>For Sale</Text>
+                </View>
+                <View style={styles.oneThirdScreen} />
+                <View style={styles.oneThirdScreen}>
+                  <Button transparent full>
+                    <Text style={styles.rightButtonText}>For Sale ></Text>
+                  </Button>
+                </View>
+              </View>
+            </Card>
+          </View>
+          </View>
+
       </View>
     );
   }
